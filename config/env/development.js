791cd05ea2d25c75755da2a592c4fad51fb457bd
amIwho <kalinon7@gmail.com>
2015-01-27 10:12:12 +0300

    some russifications

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
 	db: 'mongodb://localhost/mean-dev',
 	app: {
-		title: 'MEAN - Development Environment'
+		title: '500 слов'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
