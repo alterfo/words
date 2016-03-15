047b8ae04e9e9edae8bfbc674a532bb7057855b8
osidorkin <osidorkin@i-novus.ru>
2016-03-15 14:33:27 +0300

    fix falstart

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 cd /sites/words
-git pull
-npm install
-gulp build
 
-NODE_ENV=production forever restart server.js
+GIT_CHANGES=git pull | wc -l
+
+if [ $GIT_CHANGES -gt 1 ]; then
+    npm install
+    gulp build
+
+    NODE_ENV=production forever restart server.js
+fi
