276db1d6502f6284f7ed6e3458113d2748cba017
osidorkin <osidorkin@i-novus.ru>
2016-03-15 16:55:46 +0300

    testing session persistence

--- server.js
+++ server.js
@@ -20,10 +20,7 @@ var db = mongoose.connect(config.db, function(err) {
 	}
 });
 
-// Init the express application
 var app = require('./config/express')(db);
-
-// Bootstrap passport config
 require('./config/passport')();
 
 // Start the app by listening on <port>
