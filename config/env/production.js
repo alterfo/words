e55eea7c364b85a6b8028522447fc097518e8d7c
Oleg Sidorkin <kalinon7@gmail.com>
2017-09-20 15:28:13 +0300

    little changes from server

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+	db: 'mongodb://127.0.0.1:2017/mean',
 	assets: {
 		lib: {
 			css: [
